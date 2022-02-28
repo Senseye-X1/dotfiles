477da98bd0713a9a98bfca5f750fbda88675372e
Senseye-X1 <55647565+Senseye-X1@users.noreply.github.com>
2022-02-28 19:45:16 +0100

    Create audio-output.sh

---
+++ scripts/.scripts/audio-output.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#sinkindex=$(pacmd list-sinks | grep "* index:" | tail -c 2)
+#if (( $(pacmd list-sinks | grep "* index:" | awk '{print int($3)}') == 0))
+	#then
+		#pacmd set-default-sink 3
+##		echo ""
+	#else
+		#pacmd set-default-sink 0
+##		echo ""
+#fi
+
+if [[ "$(pactl get-default-sink)"  = "alsa_output.usb-Corsair_Simple_Audio_Listen_00000001-00.analog-stereo" ]]
+	then
+		pactl set-default-sink alsa_output.usb-SteelSeries_Arctis_Pro_Wireless-00.analog-stereo
+#		echo ""
+	else
+		pacmd set-default-sink alsa_output.usb-Corsair_Simple_Audio_Listen_00000001-00.analog-stereo
+#		echo ""
+fi
