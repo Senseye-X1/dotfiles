b0065a21fa87e4740a4b98ad555bf39e0d91fb1b
Senseye-X1 <55647565+Senseye-X1@users.noreply.github.com>
2022-03-11 18:38:21 +0100

    Create audio-icons.sh

---
+++ scripts/.local/bin/statusbar/audio-icons.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [[ "$(pactl get-default-sink)"  = "alsa_output.usb-Corsair_Simple_Audio_Listen_00000001-00.analog-stereo" ]]
+	then
+#		pactl set-default-sink alsa_output.usb-SteelSeries_Arctis_Pro_Wireless-00.analog-stereo
+    echo ""
+	else
+#		pacmd set-default-sink alsa_output.usb-Corsair_Simple_Audio_Listen_00000001-00.analog-stereo
+  	echo ""
+fi
