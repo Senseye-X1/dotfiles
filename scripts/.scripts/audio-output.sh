e90a6800e234a6f0031e64637c4454c5187e2cba
Senseye-X1 <55647565+Senseye-X1@users.noreply.github.com>
2022-03-11 20:25:57 +0100

    Update audio-output.sh

--- scripts/.scripts/audio-output.sh
+++ scripts/.scripts/audio-output.sh
@@ -15,6 +15,6 @@ if [[ "$(pactl get-default-sink)"  = "alsa_output.usb-Corsair_Simple_Audio_Liste
 		pactl set-default-sink alsa_output.usb-SteelSeries_Arctis_Pro_Wireless-00.analog-stereo
 #		echo ""
 	else
-		pacmd set-default-sink alsa_output.usb-Corsair_Simple_Audio_Listen_00000001-00.analog-stereo
+		pactl set-default-sink alsa_output.usb-Corsair_Simple_Audio_Listen_00000001-00.analog-stereo
 #		echo ""
 fi
